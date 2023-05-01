4f9054baef55a777e113efecce8a327ec39ef99d
Kash1m1r <adm1nkash1m1r@gmail.com>
2023-05-01 12:30:44 -0300

    sqlinjection

--- inseriraluno.php
+++ inseriraluno.php
@@ -6,9 +6,17 @@ require_once 'vendor/autoload.php';
 $databasePath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:' . $databasePath);
 
-$student = new Student(null, 'Gabriel Pereira', new \DateTimeImmutable('1998-11-16'));
+$student = new Student(
+     null,
+     "João', ''); DROP TABLE students; -- Dias",
+    new \DateTimeImmutable('1997-10-15')
+);
 
-$sqlInsert = "INSERT INTO students (name, birth_date) VALUES('{$student->name()}', '{$student->birthDate()->format('Y-m-d')}');";
-echo $sqlInsert . PHP_EOL;                
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (?,?);";
+$statement = $pdo->prepare($sqlInsert);
+$statement->bindValue(1, $student->name());
+$statement->bindValue(2, $student->birthDate()->format('Y-m-d'));
 
-var_dump($pdo->exec($sqlInsert));
+if($statement->execute()){
+    echo "Aluno Incluido";
+}
---
+++ listaraluno.php
@@ -0,0 +1,16 @@
+<?php
+
+use Alura\Pdo\Domain\Model\Student;
+
+require_once 'vendor/autoload.php';
+
+$databasePath = __DIR__ . '/banco.sqlite';
+$pdo = new PDO('sqlite:' . $databasePath);
+
+$statement = $pdo->query('SELECT * FROM students;');
+$studentList = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+var_dump($studentList);
+
+
+
